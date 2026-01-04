082788ee7372875a5a87976180f8fc779eb90bde
Ladipo Ezekiel Ipadeola <rileydrizzy@hotmail.com>
2026-01-04 07:07:10 +0000

    chore: update Kaggle auth configuration
    Replace Kaggle environment variables with API token required by Kaggle v1.8

--- signa2text/set_environment_variables_template.sh
+++ signa2text/set_environment_variables_template.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # replace placeholders and rename this file to `set_environment_variables.sh`
 
-#replace with Kaggle username and key
-export KAGGLE_USERNAME=username
-export KAGGLE_KEY=xxxxxxxxxxxxxx
+#replace with Kaggle API Token
+export KAGGLE_API_TOKEN=xxxxxxxxxxxxxx
 
 #replace with WANDB key
 export WANDB_API_KEY=xxxxxxxxxxxxxx
